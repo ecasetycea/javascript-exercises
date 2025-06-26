a9ae27aa6f7aa92acb11fa58ce39d9a796b49359
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-06-26 18:58:10 +0300

    07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    out = string.split('');
+    out.reverse();
+    out = out.join('');
+    return out;
 };
 
 // Do not edit below this line
