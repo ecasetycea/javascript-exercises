dd27be02c4e968499b28303b76b9e9e8635b2753
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-06-26 19:35:14 +0300

    11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  converted = (fah - 32) * 5 / 9;
+  return Math.round(10*converted) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  converted = (cel * 9 / 5) + 32
+  return Math.round(10*converted) / 10;
 };
 
 // Do not edit below this line
