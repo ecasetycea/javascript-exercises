231045739d181caef136f7cce44e47c2ddcdd8dd
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-06-26 19:22:51 +0300

    10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    out = false;
+    if( year%4 == 0 ) out = true;
+    if( year%100 == 0) out = false;
+    if( year%400 == 0) out = true;
+    return out;
 };
 
 // Do not edit below this line
