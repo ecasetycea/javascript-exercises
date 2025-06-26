d50557c6047fab024d7d2f60cd5fc9e74e85b42e
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-06-26 19:19:24 +0300

    09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(checkIllegal(a) || checkIllegal(b)) return 'ERROR';
 
+    let sum=0;
+    for(let i=Math.min(a,b); i<=Math.max(a,b); i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+function checkIllegal(a) {
+    out = false;
+    if(a<0) out=true;
+    if(a%1 !== 0) out=true;
+    if(typeof(a) !== typeof(1)) out=true;
+    return out;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
