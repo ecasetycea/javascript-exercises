2f7caaa3406e99fb46bcc34ff699031389a55c12
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-06-26 18:55:46 +0300

    06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if(repeats<0) return 'ERROR';
+    out = '';
+    for(let i=0; i<repeats; i++) {
+        out += string;
+    }
+    return out;
 };
 
 // Do not edit below this line
