1f803b3b0e87ac51ca3d0e7c1a65b0640f80cb4e
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-06-26 19:09:42 +0300

    08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, a='', b='', c='', d='') {
+    for(let i=0; i<array.length; i++) {
+        switch(array[i]) {
+            case a:
+            case b:
+            case c:
+            case d:
+                array.splice(i--,1);
+                i--;
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
