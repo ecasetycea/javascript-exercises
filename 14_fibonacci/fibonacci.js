2bccc6cc49f16dc561b780de9ab40b62e392727a
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-07-05 12:30:28 +0300

    14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(typeof number === "string") number = Number(number);
+    if(number < 0) return "OOPS";
+    
+    let a = 0;
+    let b = 1;
 
+    for(let i=0; i<number; i++) {
+        let tmp = a+b;
+        a = b;
+        b = tmp;
+    }
+
+    return a;
 };
 
 // Do not edit below this line
