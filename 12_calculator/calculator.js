f2c99e45da7bc3520b4d22474984b3df6ff60ce1
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-07-05 12:22:14 +0300

    12_calculator, 13_palindrome

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce( (cur, next) => cur + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce( (cur, next) => cur *= next, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let acc = 1;
+	for(let i=0; i<b; i++) acc *= a;
+  return acc;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let acc = 1;
+  for(let i=a; i>0; i--) acc *= i;
+  return acc;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let array = string.split('');
+    removeItemAll(array, '!');
+    removeItemAll(array, ',');
+    removeItemAll(array, '.');
+    removeItemAll(array, ' ');
 
+    for(let i=0; i<array.length; i++) {
+        if(array.at(i) !== array.at(-1-i)) return false;
+    }
+    return true;
 };
+
+function removeItemAll(array, value){
+    let i = 0;
+    while(i < array.length) {
+        if(array[i] === value) {
+            array.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
