30e24666ab23015d0829b821dde0b0f8694442b9
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-07-05 12:52:39 +0300

    16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let indexOfOldest;
+    let oldestAge = 0;
+    
+    for(person of people) {
+        let age;
+        if("yearOfDeath" in person) {
+            age = getAge(person.yearOfBirth, person.yearOfDeath);
+        } else {
+            age = getAge(person.yearOfBirth, 2025);
+        }
+        if(age > oldestAge) {
+            oldestAge = age;
+            indexOfOldest = people.indexOf(person);
+        }
 
+    }
+
+    return people[indexOfOldest];
 };
+
+function getAge(birth, death){
+    return death-birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
